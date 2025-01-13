1180e66dd95957d7854db32b4699be337c0bc64a
Andres Jimenez <andres_jimenez666@hotmail.com>
2025-01-13 12:09:44 -0500

    Back actualizado

--- backend/main.py
+++ backend/main.py
@@ -1,65 +1,126 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile
 import xmltodict
-import io
+import os
+import chardet
 
 app = FastAPI()
 
-# Configuración de CORS
+# Configurar CORS
+origins = [
+    "*" # Origen permitido para cualquier cliente
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Cambia este valor si usas otro dominio para el frontend
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos los métodos (GET, POST, etc.)
-    allow_headers=["*"],  # Permite todos los encabezados
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-# Ruta para convertir XML a JSON
 @app.post("/convertir/")
 async def convertir_xml_a_json(file: UploadFile = File(...)):
     try:
-        content = await file.read()
-        data_dict = xmltodict.parse(content)
 
-        comprobante_xml = data_dict['autorizacion']['comprobante']
-        comprobante_dict = xmltodict.parse(comprobante_xml)
-        data_dict['autorizacion']['comprobante'] = comprobante_dict
+        # Intenta leer el archivo xml, si no se puede, utiliza chardet para detectar la codificación
+        try:
+            content = await file.read()
+            content = content.decode('utf-8')
+            print(1)
+        except UnicodeDecodeError:
 
-        return JSONResponse(content=data_dict, status_code=200)
+            content = await file.read()
+            print(2)
+            encoding = chardet.detect(content)['encoding']
+            print(3)
+            content = content.decode(encoding).encode('utf-8')
+            print(4)
+
     except Exception as e:
+        print(5)
         return JSONResponse(content={"error": str(e)}, status_code=400)
+    print(6)
+    return content
 
-@app.post("/convertirExample/")
-async def convertir_xml_a_json_example(file: UploadFile = File(...)):
+    
 
-    example_dict = { 
-        "fechaEmisionDocSustento": "2023-10-01",
-        "numDocModificado": "001-001-000000001",
-        "tipoIdentificacionComprador": "05",
-        "razonSocial": "EMPRESA S.A.",
-        "codDoc": "01",
-        "estab": "001",
-        "ptoEmi": "001",
-        "secuencial": "000000001",
-        "idComprobante": "1234567890",
-        "ruc": "1234567890001",
-        "isDocente": True,
-        "fecha": "2023-10-01",
-        "formaPago": "01",
-        "formaPagoAdmitida": "01",
-        "nombre": "Juan Perez",
-        "contribuyenteRimpe": "RIMPE",
-        "fechaEmision": "2023-10-01",
-        "fechaAutorizacion": "2023-10-01T12:00:00",
-        "valor": "100.00",
-        "infoAdicional": "Información adicional",
-        "tipo": "Factura",
-        "codigoPorcentaje": "2",
-        "codigoAdmitido": "2",
-        "tipoDocumento": "01",
-        "numeroAutorizacion": "1234567890",
-        "xml_path": "/path/to/xml",
-        "pdf_path": "/path/to/pdf"
-    }
-    return JSONResponse(content=example_dict, status_code=200)
+def extract_data(data_dict):
+    try:
+        
+        comprobante = data_dict['autorizacion']['comprobante']
+
+        tipoDocumento = "factura" if "factura" in comprobante else "notaCredito" if "notaCredito" in comprobante else None
+        if not tipoDocumento:
+            raise ValueError("El tipo de documento no es válido o no se encuentra en el XML.")
+
+        contenidoComprobante = comprobante[tipoDocumento]
+
+        # Datos necesario extraidos 
+        fechaEmisionDocSustento = contenidoComprobante.get('infoNotaCredito', {}).get('fechaEmisionDocSustento', '-')
+        numDocModificado = contenidoComprobante.get('infoNotaCredito', {}).get('numDocModificado', '-')
+        tipoIdentificacionComprador = contenidoComprobante.get('infoFactura', {}).get('tipoIdentificacionComprador', '-')
+        razonSocial = contenidoComprobante.get('infoTributaria', {}).get('razonSocial', '-') if tipoDocumento == 'factura' else contenidoComprobante.get('infoTributaria', {}).get('razonSocial', '-')
+        codDoc = contenidoComprobante.get('infoTributaria', {}).get('codDoc', '-')
+        estab = contenidoComprobante.get('infoTributaria', {}).get('estab', '-')
+        ptoEmi = contenidoComprobante.get('infoTributaria', {}).get('ptoEmi', '-')
+        secuencial = contenidoComprobante.get('infoTributaria', {}).get('secuencial', '-')
+        idComprobante = f"{estab}-{ptoEmi}-{secuencial}"
+        ruc = contenidoComprobante.get('infoTributaria', {}).get('ruc', '-')
+        isDocente = contenidoComprobante.get('infoAdicional', {}).get('isDocente', '-')
+        string_fecha = contenidoComprobante.get('infoFactura', {}).get('fechaEmision', '-')
+        formaPago = contenidoComprobante.get('infoFactura', {}).get('formaPago', '-')
+        formaPagoAdmitida = contenidoComprobante.get('infoFactura', {}).get('formaPagoAdmitida', '-')
+
+        # obten ya sea razon social comprador o razon social retenido, solo exite uno
+        nombre = contenidoComprobante.get('infoFactura', {}).get('razonSocialComprador', None) or \
+                      contenidoComprobante.get('infoNotaCredito', {}).get('razonSocialComprador', None) or \
+                      contenidoComprobante.get('infoNotaCredito', {}).get('razonSocialSujetoRetenido', '-')
+
+        regimenRimpe = contenidoComprobante.get('infoTributaria', {}).get('regimenRimpe', '-')
+        fechaEmision = contenidoComprobante.get('infoFactura', {}).get('fechaEmision', '-')
+        fecha_excel = data_dict['autorizacion'].get('fechaAutorizacion', '-')
+        valor = contenidoComprobante.get('infoFactura', {}).get('valor', '-')
+        infoAdicional = contenidoComprobante.get('infoAdicional', {}).get('campoAdicional', '-')
+        tipo = contenidoComprobante.get('infoNotaCredito', {}).get('tipo', '-')
+        codigoPorcentaje = contenidoComprobante.get('infoFactura', {}).get('codigoPorcentaje', '-')
+        codigoAdmitido = contenidoComprobante.get('infoFactura', {}).get('codigoAdmitido', '-')
+        numeroAutorizacion = data_dict['autorizacion'].get('numeroAutorizacion', '-')
+        
+        # si es factura /proveedores/{razonSocial}/{estab}{ptoEmi}{secuencial}.xml y si es nota de credito /nota_credito/{razonSocial}/{estab}{ptoEmi}{secuencial}.xml
+        xml_path = f"/proveedores/{razonSocial}/{estab}{ptoEmi}{secuencial}.xml" if tipoDocumento == "factura" else f"/nota_credito/{razonSocial}/{estab}{ptoEmi}{secuencial}.xml"
+        pdf_path = f"/proveedores/{razonSocial}/{estab}{ptoEmi}{secuencial}.pdf" if tipoDocumento == "factura" else f"/nota_credito/{razonSocial}/{estab}{ptoEmi}{secuencial}.pdf"
+
+        return {
+            "fechaEmisionDocSustento": fechaEmisionDocSustento,
+            "numDocModificado": numDocModificado,
+            "tipoIdentificacionComprador": tipoIdentificacionComprador,
+            "razonSocial": razonSocial.strip().upper(),
+            "codDoc": codDoc,
+            "estab": estab,
+            "ptoEmi": ptoEmi,
+            "secuencial": secuencial,
+            "idComprobante": idComprobante,
+            "ruc": ruc,
+            "isDocente": isDocente,
+            "fecha": string_fecha,
+            "formaPago": formaPago,
+            "formaPagoAdmitida": formaPagoAdmitida,
+            "nombre": nombre if nombre else "-",
+            "contribuyenteRimpe": regimenRimpe,
+            "fechaEmision": fechaEmision,
+            "fechaAutorizacion": fecha_excel,
+            "valor": valor if valor else "-",
+            "infoAdicional": infoAdicional,
+            "tipo": tipo,
+            "codigoPorcentaje": codigoPorcentaje,
+            "codigoAdmitido": codigoAdmitido,
+            "tipoDocumento": tipoDocumento,
+            "numeroAutorizacion": numeroAutorizacion,
+            "xml_path": xml_path,
+            "pdf_path": pdf_path,
+            "raw": data_dict
+        }
+    except KeyError as e:
+        raise ValueError(f"KeyError al procesar los datos: {str(e)}")
